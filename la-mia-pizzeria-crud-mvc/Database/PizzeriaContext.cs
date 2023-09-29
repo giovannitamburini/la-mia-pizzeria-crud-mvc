03c21751d5c173e93a2bb86d4e5287a46bacf726
giovannitamburini <giovatambu@gmail.com>
2023-09-29 14:54:12 +0200

    added class pizzeriaContext & created database

---
+++ la-mia-pizzeria-crud-mvc/Database/PizzeriaContext.cs
@@ -0,0 +1,15 @@
+﻿using la_mia_pizzeria_crud_mvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_crud_mvc.Database
+{
+    public class PizzeriaContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=PizzeriaSet2023;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
--- la-mia-pizzeria-crud-mvc/Models/Pizza.cs
+++ la-mia-pizzeria-crud-mvc/Models/Pizza.cs
@@ -1,18 +1,24 @@
-﻿namespace la_mia_pizzeria_crud_mvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_crud_mvc.Models
 {
     public class Pizza
     {
         public int Id { get; set; }
+        [MaxLength(100)]
         public string Name { get; set; }
+        [Column(TypeName = "text")]
         public string Description { get; set; }
+        [MaxLength(500)]
         public string PathImage { get; set; }
         public float Price { get; set; }
 
-        public Pizza(string name, string description, string urlImage, float price)
+        public Pizza(string name, string description, string pathImage, float price)
         {
             this.Name = name;
             this.Description = description;
-            this.PathImage = urlImage;
+            this.PathImage = pathImage;
             this.Price = price;
         }
     }
