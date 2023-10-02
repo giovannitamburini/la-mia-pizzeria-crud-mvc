4acf97ec556114403d123d5957a0cd0fd99609cd
giovannitamburini <giovatambu@gmail.com>
2023-10-02 15:30:06 +0200

    added automatic validation in the Pizza model

--- la-mia-pizzeria-crud-mvc/Models/Pizza.cs
+++ la-mia-pizzeria-crud-mvc/Models/Pizza.cs
@@ -6,12 +6,23 @@ namespace la_mia_pizzeria_crud_mvc.Models
     public class Pizza
     {
         public int Id { get; set; }
+
+
         [MaxLength(100)]
+        [Required(ErrorMessage = "Il campo del nome è obbligatorio")]
         public string Name { get; set; }
+
         [Column(TypeName = "text")]
+        [Required(ErrorMessage = "Il campo della descrizione è obbligatorio")]
         public string Description { get; set; }
+
         [MaxLength(500)]
+        [Required(ErrorMessage = "Il campo del path immagine è obbligatorio")]
+        [StringLength(500, ErrorMessage = "Il path dell'immagine non può superare i 500 caratteri")]
         public string PathImage { get; set; }
+
+        [Required(ErrorMessage = "Il campo del prezzo è obbligatorio")]
+        [Range(2, 100, ErrorMessage ="Il prezzo deve essere compreso tra 2 e 100")]
         public float Price { get; set; }
 
         public Pizza() { }
