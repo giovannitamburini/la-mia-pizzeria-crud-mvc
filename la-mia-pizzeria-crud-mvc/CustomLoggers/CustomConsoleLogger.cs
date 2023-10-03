d2b9127b33927a932a8d93b2a474668155b62509
giovannitamburini <giovatambu@gmail.com>
2023-10-03 16:45:27 +0200

    added CustomConsoleLogger class

---
+++ la-mia-pizzeria-crud-mvc/CustomLoggers/CustomConsoleLogger.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace la_mia_pizzeria_crud_mvc.CustomLoggers
+{
+    public class CustomConsoleLogger
+    {
+        public void WriteLog(string message)
+        {
+            Debug.WriteLine($"LOG: {message}");
+        }
+    }
+}
